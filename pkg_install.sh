ec96a8d67da9745da07415b0c47421fb0b9f46b3
aby <anti1346@gmail.com>
2024-01-30 21:09:33 +0900

    no message

--- pkg_install.sh
+++ pkg_install.sh
@@ -2,8 +2,20 @@
 
 sudo apt-get update
 
+### sources.list.bak 파일 생성
+sudo sed -i.bak "s/\(kr\|archive\|ports\).ubuntu.com/$mirror_server/g" /etc/apt/sources.list
+
 sudo apt-get install -y lsb-release ca-certificates vim rsyslog openssh-server
 
 sudo systemctl --now enable ssh.service
 
 sudo systemctl status ssh.service
+
+### vagrant 계정 생성
+curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/vagrant_useradd.sh | bash
+
+
+
+
+
+
