3367dbd5d3f6b128120fb33abb77ff6a3a05d258
Aby <anti1346@gmail.com>
2024-01-31 16:00:00 +0900

    no message

--- install_haproxy.sh
+++ install_haproxy.sh
@@ -1,101 +1,5 @@
 #!/bin/bash
 
-#### COMMON
-sudo sed -i.bak "s/\(kr\|archive\).ubuntu.com/mirror.kakao.com/g" /etc/apt/sources.list
-sudo apt-get update
-
-# 필수 패키지 설치
-if ! dpkg -l | grep -q openssh-server; then
-    sudo apt-get install -y lsb-release ca-certificates vim rsyslog openssh-server
-    sudo systemctl restart rsyslog
-    sudo systemctl --now enable ssh.service
-    sudo systemctl restart ssh.service
-fi
-
-# keepalived 패키지 설치
-if ! dpkg -l | grep -q keepalived; then
-    sudo apt-get install -y keepalived
-    sudo systemctl --now enable keepalived.service
-    sudo systemctl restart keepalived.service
-    generate_keepalived_config
-fi
-
-# haproxy 패키지 설치
-if ! dpkg -l | grep -q haproxy; then
-    sudo apt-get install -y haproxy
-    sudo systemctl --now enable haproxy.service
-    sudo systemctl restart haproxy.service
-    generate_haproxy_config
-fi
-
-# vagrant 계정이 존재하지 않으면 vagrant_useradd.sh 스크립트 실행
-if ! id "vagrant" &>/dev/null; then
-    curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/vagrant_useradd.sh | bash
-fi
-
-if [ "$HOSTNAME" == "haproxy01" ]; then
-    #### haproxy01
-    #ssh-keygen -t rsa -b 2048 -C "deployment" -f /root/.ssh/id_rsa -N ""
-    if [ ! -f "/root/.ssh/id_rsa" ]; then
-        mkdir -m 700 /root/.ssh
-        # 함수 호출하여 id_rsa 파일 생성
-        generate_ssh_private_key
-    fi
-    
-    if [ ! -d "/etc/ssl/ha_sangchul_kr" ]; then
-        mkdir -p /etc/ssl/ha_sangchul_kr
-        cd /etc/ssl/ha_sangchul_kr
-        openssl req \
-        -newkey rsa:4096 \
-        -x509 \
-        -sha256 \
-        -days 3650 \
-        -nodes \
-        -out ha_sangchul_kr.crt \
-        -keyout ha_sangchul_kr.key \
-        -subj "/C=KR/ST=Seoul/L=Jongno-gu/O=SangChul Co., Ltd./OU=Infrastructure Team/CN=ha.sangchul.kr"
-        cat ha_sangchul_kr.key ha_sangchul_kr.crt > unified_ha_sangchul_kr.pem
-    fi
-fi
-
-if [ "$HOSTNAME" == "haproxy02" ]; then
-    #### haproxy02
-    if [ ! -d "/root/.ssh" ]; then
-        mkdir -m 700 /root/.ssh
-        # 함수 호출하여 authorized_keys 파일에 공개 키 추가
-        add_ssh_authorized_key
-    fi
-
-    if [ ! -d "/etc/ssl/ha_sangchul_kr" ]; then
-        mkdir -p /etc/ssl/ha_sangchul_kr
-    fi
-
-    sudo sed -i.bak "s/state MASTER/state BACKUP/g" /etc/keepalived/keepalived.conf
-    sudo sed -i.bak "s/priority 100/priority 99/g" /etc/keepalived/keepalived.conf
-
-fi
-
-sudo systemctl status rsyslog.service
-sudo systemctl status ssh.service
-openssl x509 -in /etc/ssl/ha_sangchul_kr/ha_sangchul_kr.crt -noout -subject -dates
-
-# Check keepalived configuration
-if ! keepalived -t &>/dev/null; then
-    sudo systemctl restart keepalived.service
-fi
-
-# Check HAProxy configuration
-if ! haproxy -c -f /etc/haproxy/haproxy.cfg -V &>/dev/null; then
-    sudo systemctl restart haproxy.service
-fi
-
-echo -e "\n### SSL CERT COPY"
-echo "scp /etc/ssl/ha_sangchul_kr/unified_ha_sangchul_kr.pem root@172.19.0.3:/etc/ssl/ha_sangchul_kr/unified_ha_sangchul_kr.pem"
-
-
-
-
-
 ###############################################################################################################################
 ###############################################################################################################################
 ###############################################################################################################################
@@ -103,42 +7,45 @@ echo "scp /etc/ssl/ha_sangchul_kr/unified_ha_sangchul_kr.pem root@172.19.0.3:/et
 ###############################################################################################################################
 ###############################################################################################################################
 ###############################################################################################################################
+
 generate_ssh_private_key() {
-    cat <<EOF > /root/.ssh/id_rsa
-    -----BEGIN OPENSSH PRIVATE KEY-----
-    b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABFwAAAAdzc2gtcn
-    NhAAAAAwEAAQAAAQEA4X3ocCY57yPxHyS/jzsSByaGrLj+5O1zwwkUEqVRPaHDi6R0EvS7
-    d0WRW82IxebZWpABUKrpZjwHQZVvYACjtGPnBTQxbAW95QOlXEtglSdtPCS3b48Fb8X7oc
-    wbU91HQaWKc0rQ9lTUP+Amh/m7sg1tIGZC/PneKY5yi0uaa4k52FOzRHWsU0CWcAyBnnFX
-    hftXGXcfuDQsCTVpEDkvb8kpp6xhQBcSnSNIF7qkSpD0oXBpbcbCdGPwgCp/LIM0G4c6qF
-    cmSnqM2L3Xp8ZSfj+xkH/8kA3bqMJvfHA++TUc9B2pBuL18TLP0mU3QZIdNFqvmSBsYdUT
-    FG/HX7faYwAAA8CgpohQoKaIUAAAAAdzc2gtcnNhAAABAQDhfehwJjnvI/EfJL+POxIHJo
-    asuP7k7XPDCRQSpVE9ocOLpHQS9Lt3RZFbzYjF5tlakAFQqulmPAdBlW9gAKO0Y+cFNDFs
-    Bb3lA6VcS2CVJ208JLdvjwVvxfuhzBtT3UdBpYpzStD2VNQ/4CaH+buyDW0gZkL8+d4pjn
-    KLS5priTnYU7NEdaxTQJZwDIGecVeF+1cZdx+4NCwJNWkQOS9vySmnrGFAFxKdI0gXuqRK
-    kPShcGltxsJ0Y/CAKn8sgzQbhzqoVyZKeozYvdenxlJ+P7GQf/yQDduowm98cD75NRz0Ha
-    kG4vXxMs/SZTdBkh00Wq+ZIGxh1RMUb8dft9pjAAAAAwEAAQAAAQBAhsk3HplBh6V+ZgOz
-    NxInnay4TJAUbqbLzxNBarFe06WjlkHpEsN6lBvOi3hyOWdFdSQLM31q1g61g8/FRymRe2
-    0mnhLueI4otOxjBubyh7/IkDE16VWC8MLbQA8p5o53iKmf6G73rrq2NKySCLLfdtwdg2X0
-    AoTCm5LHrbDYyHh8ABi3ifwBWn3lu3XDmA/UQWgOffW556L9KUlNQwwq6N8zX+ADMZpLCd
-    rE/czveBPkJTqOfdoZ4R2RWJEyeP2JZU7L3ubXkz5smrpq8d9XwJRx8YOMTeqe9XiByHp/
-    ugIwmNnP/VHb9ChshvYfAVqS/iujykqAvjxCWZq56I3RAAAAgQCKmvDS3W7SijupFN5Um/
-    z+zXfqiGilT9toAEEwpgKdS822j3Ritwnc4mxJvl4yR7+Wg+4kFy4bxBw+tDNJJ0YKkA7U
-    D2skb6tKmarxxSRqQdrBTjY8Ro1LOS3MFxsEIQkGVqEz7RXUqzxlfwn/XRr+8pfyhRVhpX
-    fBzaB1yvm1YAAAAIEA52HBBC66G93JcC/nkm4IIr8Qvx5CvXDHgd+9f0c0gSQFm6lrD90H
-    DSLpFcZ/hxWTFsrBHbNTghw0YPM6MDKeYqkVDwQj9mdulAUHWh2G4gzHPGgbXvmO5MmkgR
-    9TB78ZdhzaqLIgnM/C1jAIAafsa7/HYwNwybJTnYsMAg4qIG0AAACBAPl7uZPs74Yl7mjj
-    jHBOxZmfOzxxY8ZymIZjEcZCZJ+wykTnzJZ/g+ukYLh9SWS3xrNGB5PAc2g1FmyZYtP/u8
-    /ptjqWD+6Tp501G0Vp+Wic7z2s4nO8+FvKcu0JQoE8nN4k5CASDDnvh/6l8usYvWVj92Aa
-    +EBxaduGTn0dT0QPAAAACmRlcGxveW1lbnQ=
-    -----END OPENSSH PRIVATE KEY-----
+    cat <<EOF > $HOME/.ssh/id_rsa
+-----BEGIN OPENSSH PRIVATE KEY-----
+b3BlbnNzaC1rZXktdjEAAAAABG5vbmUAAAAEbm9uZQAAAAAAAAABAAABFwAAAAdzc2gtcn
+NhAAAAAwEAAQAAAQEAzjj1aU9WovLI/dGA6Ambo2Dsn3+dBAOwSR5uUGXFzEP+u9B+OlFS
+V4jd7TE5TLv/3GimTgVOh00Qh4OQoPgg22I3jgYBgjkhJPZMTZkbBNqwoXXkBS4WgnAeeh
+CmMhyfbmKW3u9jHFlrM0wx6bXskpQaRP+2I53tg/U90y0p8Md5HSGdceCerccLcNwYkip/
+8FcXMV+SZhwH/ha+l69bJDrWNNYpCGn1l7Nd0RUcy3yL611IZnHyKcEi30YmN+rjrJEi6J
+syubpKijKPDKhR9q8WyUl1m6Qe56tVSlvvFD3lQ25QF4+/ZLp9ofKwE9U8GFBBSYnpRX5e
+NA23q9qVOQAAA8AhJ4/TISeP0wAAAAdzc2gtcnNhAAABAQDOOPVpT1ai8sj90YDoCZujYO
+yff50EA7BJHm5QZcXMQ/670H46UVJXiN3tMTlMu//caKZOBU6HTRCHg5Cg+CDbYjeOBgGC
+OSEk9kxNmRsE2rChdeQFLhaCcB56EKYyHJ9uYpbe72McWWszTDHpteySlBpE/7Yjne2D9T
+3TLSnwx3kdIZ1x4J6txwtw3BiSKn/wVxcxX5JmHAf+Fr6Xr1skOtY01ikIafWXs13RFRzL
+fIvrXUhmcfIpwSLfRiY36uOskSLomzK5ukqKMo8MqFH2rxbJSXWbpB7nq1VKW+8UPeVDbl
+AXj79kun2h8rAT1TwYUEFJielFfl40Dber2pU5AAAAAwEAAQAAAQAF3wLu+xvFKt1Ztepn
+cMfraFnZlqQClmpL7UpduUVe3JFhiSeMLdQxesuQB7E7w98ecs+dSV25e2eyO/wqS7Yqbi
+A/x7Wo8/XDcXcC/zi7iXsINcIxUDptf0IBiVIkpheh6GZRituLAJQNPf5DyvKlH635yJVH
+wU90TM9JoQULKPg6+2u50dJOgKvqocXM29MK+XRazm9yesC6f4lP7X5PzqDpaQ+Oicvf7H
+X9W3yOpE3uD8wnvsUQ7lPtMyZ2dD34q+4CqtDQnf5UPv6LJxVrPYfbLgdsNzmKLxdjqltG
+6XeZD9eWL4MAsqDZJ7wOZvhuP6XkddkGaj3bbnnQGjhpAAAAgCCs3SgG1ppxgnJcVqgHf/
+RchKxhgoq0o81YIiPvlVunYrdd9FtZyTGX2gSVRIfTRPMwEyakN/7v0Y1HxQKL43BZxJj8
+qf1edpxY7pMF0IDN0Cbq8aDU2fXgUgTLc3ZBMM8kFzMWPR9DYFKOmh97YWWLAvyWSMmMOh
+My4WnTpVGiAAAAgQD3PWqavEDIKnjwN40k/i4ulbELu2bc7pPltAcQ7U4kO5Li/DcgaYbv
+npCLMdIfgbZxiYOyy/8LI3/i5Ov334BMusg6NdH17PRKNOjI6l4X5wUKg2eY7dBo0E34k8
+6uYV/TJ1nSfp05oFR1Jh7bmH3wvgK4Xngxj/ppF9CLLNO4rQAAAIEA1Yd+qOY2A2z3eRkz
+FeJKCNixncDSgioNd8GBvm6nbh13B5c0UPWY2oSUsPDJe+jzGJ3v34cRTr797NQVq8gG1G
+DGDGdktw7XNHXxseYne0HdcMTbiVzPg65ojp7CRGOk8l2mIqCYK6/SpZD9wziWaIY4YtRI
+goGjeiD4+QLUZD0AAAAKZGVwbG95bWVudAE=
+-----END OPENSSH PRIVATE KEY-----
 EOF
+    chmod 600 $HOME/.ssh/id_rsa
 }
 
 add_ssh_authorized_key() {
-    cat <<EOF >> /root/.ssh/authorized_keys
-    ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDhfehwJjnvI/EfJL+POxIHJoasuP7k7XPDCRQSpVE9ocOLpHQS9Lt3RZFbzYjF5tlakAFQqulmPAdBlW9gAKO0Y+cFNDFsBb3lA6VcS2CVJ208JLdvjwVvxfuhzBtT3UdBpYpzStD2VNQ/4CaH+buyDW0gZkL8+d4pjnKLS5priTnYU7NEdaxTQJZwDIGecVeF+1cZdx+4NCwJNWkQOS9vySmnrGFAFxKdI0gXuqRKkPShcGltxsJ0Y/CAKn8sgzQbhzqoVyZKeozYvdenxlJ+P7GQf/yQDduowm98cD75NRz0HakG4vXxMs/SZTdBkh00Wq+ZIGxh1RMUb8dft9pj deployment
+    cat <<EOF >> $HOME/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOOPVpT1ai8sj90YDoCZujYOyff50EA7BJHm5QZcXMQ/670H46UVJXiN3tMTlMu//caKZOBU6HTRCHg5Cg+CDbYjeOBgGCOSEk9kxNmRsE2rChdeQFLhaCcB56EKYyHJ9uYpbe72McWWszTDHpteySlBpE/7Yjne2D9T3TLSnwx3kdIZ1x4J6txwtw3BiSKn/wVxcxX5JmHAf+Fr6Xr1skOtY01ikIafWXs13RFRzLfIvrXUhmcfIpwSLfRiY36uOskSLomzK5ukqKMo8MqFH2rxbJSXWbpB7nq1VKW+8UPeVDblAXj79kun2h8rAT1TwYUEFJielFfl40Dber2pU5 deployment
 EOF
+    chmod 400 $HOME/.ssh/authorized_keys
 }
 
 generate_keepalived_config() {
@@ -244,3 +151,115 @@ listen stats
     stats refresh 10s
 EOF
 }
+
+###############################################################################################################################
+###############################################################################################################################
+###############################################################################################################################
+########################################################## 함수 파일 ###########################################################
+###############################################################################################################################
+###############################################################################################################################
+###############################################################################################################################
+
+#### COMMON
+sudo sed -i.bak "s/\(kr\|archive\).ubuntu.com/mirror.kakao.com/g" /etc/apt/sources.list
+sudo apt-get update
+
+# 필수 패키지 설치
+if ! dpkg -l | grep -q openssh-server; then
+    sudo apt-get install -y lsb-release ca-certificates vim rsyslog openssh-server
+    sudo systemctl restart rsyslog
+    sudo systemctl --now enable ssh.service
+    sudo sed -i.bak 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sudo systemctl restart ssh.service
+fi
+
+# keepalived 패키지 설치
+if ! dpkg -l | grep -q keepalived; then
+    sudo apt-get install -y keepalived
+    sudo systemctl --now enable keepalived.service
+    sudo systemctl restart keepalived.service
+    generate_keepalived_config
+fi
+
+# haproxy 패키지 설치
+if ! dpkg -l | grep -q haproxy; then
+    sudo apt-get install -y haproxy
+    sudo systemctl --now enable haproxy.service
+    sudo systemctl restart haproxy.service
+    generate_haproxy_config
+fi
+
+# vagrant 계정이 존재하지 않으면 vagrant_useradd.sh 스크립트 실행
+if ! id "vagrant" &>/dev/null; then
+    curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/vagrant_useradd.sh | bash
+fi
+
+if [ "$HOSTNAME" == "haproxy01" ]; then
+    #### haproxy01
+    #ssh-keygen -t rsa -b 2048 -C "deployment" -f $HOME/.ssh/id_rsa -N ""
+    if [ ! -f "$HOME/.ssh/id_rsa" ]; then
+        mkdir -m 700 $HOME/.ssh
+        # 함수 호출하여 id_rsa 파일 생성
+        generate_ssh_private_key
+        echo "$(whoami):$(whoami)" | chpasswd
+    fi
+    
+    if [ ! -d "/etc/ssl/ha_sangchul_kr" ]; then
+        mkdir -p /etc/ssl/ha_sangchul_kr
+        cd /etc/ssl/ha_sangchul_kr
+        openssl req \
+        -newkey rsa:4096 \
+        -x509 \
+        -sha256 \
+        -days 3650 \
+        -nodes \
+        -out ha_sangchul_kr.crt \
+        -keyout ha_sangchul_kr.key \
+        -subj "/C=KR/ST=Seoul/L=Jongno-gu/O=SangChul Co., Ltd./OU=Infrastructure Team/CN=ha.sangchul.kr"
+        cat ha_sangchul_kr.key ha_sangchul_kr.crt > unified_ha_sangchul_kr.pem
+    fi
+
+    echo -e "\n### SSL CERT DATES"
+    openssl x509 -in /etc/ssl/ha_sangchul_kr/ha_sangchul_kr.crt -noout -subject -dates
+
+    echo -e "\n### SSL CERT COPY"
+    echo "scp -o StrictHostKeyChecking=no /etc/ssl/ha_sangchul_kr/unified_ha_sangchul_kr.pem root@172.19.0.3:/etc/ssl/ha_sangchul_kr/unified_ha_sangchul_kr.pem"
+fi
+
+if [ "$HOSTNAME" == "haproxy02" ]; then
+    #### haproxy02
+    if [ ! -f "$HOME/.ssh/authorized_keys" ]; then
+        mkdir -m 700 $HOME/.ssh
+        # 함수 호출하여 authorized_keys 파일에 공개 키 추가
+        add_ssh_authorized_key
+        echo "$(whoami):$(whoami)" | chpasswd
+    fi
+
+    if [ ! -d "/etc/ssl/ha_sangchul_kr" ]; then
+        mkdir -p /etc/ssl/ha_sangchul_kr
+    fi
+
+    sudo sed -i.bak "s/state MASTER/state BACKUP/g" /etc/keepalived/keepalived.conf
+    sudo sed -i.bak "s/priority 100/priority 99/g" /etc/keepalived/keepalived.conf
+fi
+
+echo -e "\n### rsyslog status"
+sudo systemctl status rsyslog.service
+
+echo -e "\n### ssh status"
+sudo systemctl status ssh.service
+
+# Check keepalived configuration
+echo -e "\n### keepalived restart"
+if keepalived -t &>/dev/null; then
+    sudo systemctl restart keepalived.service
+    sudo systemctl status keepalived.service
+fi
+
+# Check HAProxy configuration
+echo -e "\n### haproxy restart"
+if haproxy -c -f /etc/haproxy/haproxy.cfg -V &>/dev/null; then
+    sudo systemctl restart haproxy.service
+    sudo systemctl status haproxy.service
+fi
+
--- install_webserver.sh
+++ install_webserver.sh
@@ -1,34 +1,5 @@
 #!/bin/bash
 
-#### COMMON
-sudo sed -i.bak "s/\(kr\|archive\).ubuntu.com/mirror.kakao.com/g" /etc/apt/sources.list
-sudo apt-get update
-
-# 필수 패키지 설치
-if ! dpkg -l | grep -q openssh-server; then
-    sudo apt-get install -y lsb-release ca-certificates vim rsyslog openssh-server
-    sudo systemctl restart rsyslog
-    sudo systemctl --now enable ssh.service
-    sudo systemctl restart ssh.service
-fi
-
-# nginx 패키지 설치
-if ! dpkg -l | grep -q haproxy; then
-    sudo apt-get install -y nginx php-fpm
-    sudo systemctl --now enable nginx
-    sudo systemctl --now enable php8.1-fpm
-    generate_index_html
-fi
-
-# vagrant 계정이 존재하지 않으면 vagrant_useradd.sh 스크립트 실행
-if ! id "vagrant" &>/dev/null; then
-    curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/vagrant_useradd.sh | bash
-fi
-
-
-
-
-
 ###############################################################################################################################
 ###############################################################################################################################
 ###############################################################################################################################
@@ -36,6 +7,7 @@ fi
 ###############################################################################################################################
 ###############################################################################################################################
 ###############################################################################################################################
+
 generate_index_html() {
     cat <<EOF > /usr/share/nginx/html/index.html
 <!DOCTYPE html>
@@ -54,4 +26,72 @@ generate_index_html() {
     </body>
 </html>
 EOF
+
+    ln -s /usr/share/nginx/html/index.html /var/www/html/index.html
 }
+
+add_ssh_authorized_key() {
+    cat <<EOF >> $HOME/.ssh/authorized_keys
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDOOPVpT1ai8sj90YDoCZujYOyff50EA7BJHm5QZcXMQ/670H46UVJXiN3tMTlMu//caKZOBU6HTRCHg5Cg+CDbYjeOBgGCOSEk9kxNmRsE2rChdeQFLhaCcB56EKYyHJ9uYpbe72McWWszTDHpteySlBpE/7Yjne2D9T3TLSnwx3kdIZ1x4J6txwtw3BiSKn/wVxcxX5JmHAf+Fr6Xr1skOtY01ikIafWXs13RFRzLfIvrXUhmcfIpwSLfRiY36uOskSLomzK5ukqKMo8MqFH2rxbJSXWbpB7nq1VKW+8UPeVDblAXj79kun2h8rAT1TwYUEFJielFfl40Dber2pU5 deployment
+EOF
+    chmod 400 $HOME/.ssh/authorized_keys
+}
+
+###############################################################################################################################
+###############################################################################################################################
+###############################################################################################################################
+########################################################## 함수 파일 ###########################################################
+###############################################################################################################################
+###############################################################################################################################
+###############################################################################################################################
+
+#### COMMON
+sudo sed -i.bak "s/\(kr\|archive\).ubuntu.com/mirror.kakao.com/g" /etc/apt/sources.list
+sudo apt-get update
+
+# 필수 패키지 설치
+if ! dpkg -l | grep -q openssh-server; then
+    sudo apt-get install -y lsb-release ca-certificates vim rsyslog openssh-server
+    sudo systemctl restart rsyslog
+    sudo systemctl --now enable ssh.service
+    sudo sed -i.bak 's/^#PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+    sudo systemctl restart ssh.service
+fi
+
+if [ "$HOSTNAME" == "web01" -o "$HOSTNAME" == "web02" ]; then
+    #### web01, web02
+    if [ ! -f "$HOME/.ssh/authorized_keys" ]; then
+        mkdir -m 700 $HOME/.ssh
+        # 함수 호출하여 authorized_keys 파일에 공개 키 추가
+        add_ssh_authorized_key
+    fi
+
+    echo "$(whoami):$(whoami)" | chpasswd
+fi
+
+# nginx 패키지 설치
+if ! dpkg -l | grep -q haproxy; then
+    sudo apt-get install -y nginx php-fpm
+    sudo systemctl --now enable nginx
+    sudo systemctl --now enable php8.1-fpm
+    generate_index_html
+fi
+
+# vagrant 계정이 존재하지 않으면 vagrant_useradd.sh 스크립트 실행
+if ! id "vagrant" &>/dev/null; then
+    curl -fsSL https://raw.githubusercontent.com/anti1346/zz/main/etc/vagrant_useradd.sh | bash
+fi
+
+# Check nginx configuration
+echo -e "\n### nginx restart"
+if nginx -t &>/dev/null; then
+    sudo systemctl restart nginx.service
+    sudo systemctl status nginx.service
+fi
+
+# Check php8.1-fpm configuration
+echo -e "\n### php8.1-fpm restart"
+if php-fpm8.1 -t &>/dev/null; then
+    sudo systemctl restart php8.1-fpm.service
+    sudo systemctl status php8.1-fpm.service
+fi
